b65a2f077984c03ae65cd39b67c0bc914b3fe98b
Erdem193 <98175885+Erdem193@users.noreply.github.com>
2022-02-21 20:40:38 +0300

    Add files via upload

---
+++ deneme.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+
+int main(){
+  int x=0;
+  int adet=0;
+  for(int j=1;j<=100;j++){
+    x+=1;
+    if(x%3==0&& x%7!=0){
+      adet+=1;
+    }
+  }
+  cout<<"3'e Bolunen 7'ye Bolunmeyen Sayi Adedi:"<<adet;
+}
